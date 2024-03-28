5e15bb277f238262ed980bf1b12011ecd498059c
Sam Winant <sam.winant@student.kuleuven.be>
2024-03-28 08:41:58 +0100

    added the servername as parameter

--- dsgt1-rmi/src/client.java
+++ dsgt1-rmi/src/client.java
@@ -5,15 +5,14 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class client {
-
-    private void Client() {}
-
     public static void main(String[] args) {
+
+
         try {
             Registry registry = LocateRegistry.getRegistry(args[0]);
-            Manager stub = (Manager)registry.lookup("Manager");
+            Manager stub = (Manager) registry.lookup("Manager");
             new BookingClient(stub);
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
         }
--- dsgt1-rmi/src/server.java
+++ dsgt1-rmi/src/server.java
@@ -1,8 +1,10 @@
 import hotel.BookingManager;
 import hotel.Manager;
 
+import java.lang.reflect.Field;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.RemoteObject;
 import java.rmi.server.UnicastRemoteObject;
 
 public class server {
@@ -10,7 +12,7 @@ public class server {
 
         try {
             BookingManager bm = new BookingManager();
-            Manager stub = (Manager) UnicastRemoteObject.exportObject(bm, 0);
+            Manager stub = (Manager) UnicastRemoteObject.exportObject(bm, 9090);
             Registry registry = LocateRegistry.getRegistry();
             registry.bind("Manager", stub);
             System.err.println("Server ready");
