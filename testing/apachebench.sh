c9f121f496468e64d7b9cc0e4de7439f110bcf59
Jens <jens.coosemans@student.kuleuven.be>
2024-03-30 18:20:08 +0100

    add first tests

---
+++ testing/apachebench.sh
@@ -0,0 +1,1 @@
+ab -n 100000 -c 99 -g results.tsv -v 4 'http://dsgt2024team13.japaneast.cloudapp.azure.com:7070/restrpc/meals'
--- testing/periodic_parallel.sh
+++ testing/periodic_parallel.sh
@@ -1,3 +1,5 @@
-seq 6000 | xargs -n 1 -P 100 -I {} \
-sh -c 'curl -X GET dsgt.uksouth.cloudapp.azure.com:7070/restrpc/meals \
--H "Content-type:application/json"; sleep 0.5'
+seq 10000 | xargs -n 1 -P 99 -I {} \
+sh -c 'retry=3; while [ $retry -gt 0 ]; do \
+echo "Making request {}"; \
+curl -X GET dsgt2024team13.japaneast.cloudapp.azure.com:7070/restrpc/meals -H "Content-type:application/json" && break; \
+retry=$(($retry - 1)); sleep 1; done'
