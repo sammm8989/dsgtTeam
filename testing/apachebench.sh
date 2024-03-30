916bb6279a15cb7301a6e0efff38480d86c04436
Jens <jens.coosemans@student.kuleuven.be>
2024-03-30 21:34:40 +0100

    tests for Japan

--- testing/apachebench.sh
+++ testing/apachebench.sh
@@ -1,1 +1,1 @@
-ab -n 100000 -c 99 -g results.tsv -v 4 'http://dsgt2024team13.japaneast.cloudapp.azure.com:7070/restrpc/meals'
+ab -n 40000 -c 100 -g japan_100_40000.tsv 'http://dsgt2024team13.japaneast.cloudapp.azure.com:7070/restrpc/meals'
