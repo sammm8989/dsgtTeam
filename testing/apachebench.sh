5ac1902840b02dabac4c834316c138d490a7c705
Jens <jens.coosemans@student.kuleuven.be>
2024-04-01 00:52:12 +0200

    creating order in the files (and add all for 10k requests)

--- testing/apachebench.sh
+++ testing/apachebench.sh
@@ -1,1 +1,1 @@
-ab -n 10000 -c 100 -g japan_100_10000.tsv 'http://dsgt.canadacentral.cloudapp.azure.com:7070/restrpc/meals'
+ab -n 10000 -c 75 -g japan_75_10000.tsv 'http://dsgt.canadacentral.cloudapp.azure.com:7070/restrpc/meals'
