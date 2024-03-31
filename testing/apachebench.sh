8ccbc3c34d262ebdc16e67ccba3da57c45c52b28
Jens <jens.coosemans@student.kuleuven.be>
2024-03-31 23:01:14 +0200

    Moooore data

--- testing/apachebench.sh
+++ testing/apachebench.sh
@@ -1,1 +1,1 @@
-ab -n 25000 -c 75 -g canada_75_25000.tsv 'http://dsgt.canadacentral.cloudapp.azure.com:7070/restrpc/meals'
+ab -n 10000 -c 100 -g japan_100_10000.tsv 'http://dsgt.canadacentral.cloudapp.azure.com:7070/restrpc/meals'
