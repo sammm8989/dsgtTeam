0cace9d666f2cd10a1fe6ba71ce4603d267da425
Jens <jens.coosemans@student.kuleuven.be>
2024-03-31 21:57:32 +0200

    add a lot of files from tests

--- testing/apachebench.sh
+++ testing/apachebench.sh
@@ -1,1 +1,1 @@
-ab -n 40000 -c 100 -g japan_100_40000.tsv 'http://dsgt2024team13.japaneast.cloudapp.azure.com:7070/restrpc/meals'
+ab -n 25000 -c 75 -g japan_75_25000.tsv 'http://dsgt2024team13.japaneast.cloudapp.azure.com:7070/restrpc/meals'
